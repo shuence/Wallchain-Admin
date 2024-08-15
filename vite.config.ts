903f681527456c629dfe964292af3cb0721571f5
shuence <Shubhampitekar98@gmail.com>
2024-08-16 00:10:36 +0530

    Server Side Rendering

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import path from 'path'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import ssr from 'vite-plugin-ssr/plugin';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [ssr()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
